ea639e06b8fa6618ae005d669d88f2d9695170f5
DiegoMartinez~ <diegofernandomartinezcalderon@gmail.com>
2023-09-20 16:45:14 -0500

    feat: :sparkles: implementation of links
    implementation of an example of routers with react-router-dom

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function Home() {
+  return (
+        <> 
+        <Link to={"/registroAdm"} > <button>Registro</button></Link>
+        <button>Ingreso</button> 
+       
+        </>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
+import Home from './Components/Home/Home.jsx'
 import RegistroForm from './Components/login/registro.jsx'
+
+const router =createBrowserRouter([
+  {
+    path:'/',
+    element: <Home/>
+  },
+  {
+    path:'/registroAdm',
+    element: <RegistroForm/>
+  }
+])
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RegistroForm />
+  <RouterProvider router={router}/>
   </React.StrictMode>,
 )

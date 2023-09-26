139378199339998e1baab85da9d8c9aa4bf8ea16
DiegoMartinez~ <diegofernandomartinezcalderon@gmail.com>
2023-09-26 14:32:26 -0500

    refactor: :recycle: change the way to use imagen error404
    use import to implement th error404 img

--- src/Components/NotFound/NotFound.jsx
+++ src/Components/NotFound/NotFound.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Link } from "react-router-dom";
 import './NotFound.css'
+import ImgError404 from '../../../public/Error404.png'
 
 export default function NotFound() {
   return (
-    <> <div> <img src="../../../public/error404.png" id='IMGerror404' alt="Texto alternativo de la imagen" div/></div>
+    <> <div> <img src={ImgError404} id='IMGerror404' alt="Texto alternativo de la imagen" div/></div>
 
       <Link to={"/"}>
       <button id='btnInicio' >INICIO</button>
